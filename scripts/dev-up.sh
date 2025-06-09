7a669c5eb6837edc9b083fd6987837909fed0d9e
Matheus Marciano <mm.leitea@outlook.com>
2025-06-09 19:24:50 +0000

    🧑‍💻 Rename project folder and add restart

--- scripts/dev-up.sh
+++ scripts/dev-up.sh
@@ -2,7 +2,7 @@ export DUID=$(id -u)
 export DGID=$(id -g)
 
 export USER=${USER}
+export FOLDER=$(basename $(pwd))
 
 mkdir -p .vscode-server
-
 docker compose -f compose-dev.yml up -d --build --no-deps
